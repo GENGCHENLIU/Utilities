40a84229481a604fdb2ab3e2b6bc3fafafeb177f
liueri19 <liueri19@7hills.org>
2018-05-07 09:39:44 -0400

    fleshed out CSVFile readFile and parse CSV line

--- src/csvio/CSVFile.java
+++ src/csvio/CSVFile.java
@@ -1,14 +1,13 @@
 package csvio;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Represents a CSV file.
  */
-public class CSVFile {
+public final class CSVFile implements Iterable<List<String>> {
+
 	private final List<List<String>> contents = new ArrayList<>();
 
 	/**
@@ -21,22 +20,46 @@ public class CSVFile {
 	 * @param contents a 2-dimensional collection with each sub collection representing
 	 *                 a row of the CSV file.
 	 */
-	public CSVFile(Collection<? extends Collection<?>> contents) {
-		for (Collection<?> row : contents) {
-			final List<String> sRow = new ArrayList<>();
-
-			for (Object e : row)
-				sRow.add(e != null ? e.toString() : "null");
-
-			this.contents.add(sRow);
-		}
-	}
+	public CSVFile(Collection<? extends Collection<?>> contents) { addRows(contents); }
 
 
 	/**
 	 * Returns an unmodifiable view of the specified row.
 	 */
-	public List<String> getRow(int row) {
-		return Collections.unmodifiableList(contents.get(row));
+	public List<String> getRow(int index) {
+		return Collections.unmodifiableList(contents.get(index));
 	}
+
+	/**
+	 * Adds a row to the end of this document.
+	 */
+	public boolean addRow(Collection<?> row) {
+		return contents.add(toStringList(row));
+	}
+
+	/**
+	 * Inserts a row at the specified index.
+	 */
+	public void insertRow(int index, Collection<?> row) {
+		contents.add(index, toStringList(row));
+	}
+
+	public void addRows(Collection<? extends Collection<?>> contents) {
+		for (Collection<?> row : contents)
+			addRow(row);
+	}
+
+	public List<String> removeRow(int index) {
+		return contents.remove(index);
+	}
+
+
+	private static List<String> toStringList(Collection<?> c) {
+		return c.stream()
+				.map(Objects::toString)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Iterator<List<String>> iterator() { return contents.iterator(); }
 }
--- src/csvio/CSVFiles.java
+++ src/csvio/CSVFiles.java
@@ -1,7 +1,73 @@
 package csvio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Utility methods for reading an writing CSV files.
  */
-public class CSVFiles {
+public final class CSVFiles {
+	private CSVFiles() {}
+
+	/**
+	 * Reads the file at the specified Path as a CSV file.
+	 */
+	public static CSVFile readFile(Path file) throws IOException {
+		final CSVFile csvFile = new CSVFile();
+
+		Files.readAllLines(file).stream()
+				.map(CSVFiles::parseRow)
+				.forEach(csvFile::addRow);
+
+		return csvFile;
+	}
+
+	/**
+	 * Splits a line in a CSV file to a list of entries, parsing any quotes if needed.
+	 */
+	private static List<String> parseRow(String s) {
+		final List<String> row = new ArrayList<>();
+
+		try {
+			final StringBuilder buffer = new StringBuilder();
+			final char[] chars = s.toCharArray();
+			boolean inQuotes = false;
+
+			for (int i = 0; i < chars.length; i++) {
+				final char c = chars[i];
+
+				if (c == '\"') {	// double quote
+					inQuotes = !inQuotes;
+					continue;
+				}
+
+				if (c == '\\') {	// escape
+					buffer.append(chars[++i]);	// add next char
+					continue;
+				}
+
+				if (c == ',') {
+					row.add(buffer.toString());
+					buffer.delete(0, buffer.length());
+					continue;
+				}
+
+				buffer.append(c);
+			}
+		}
+		catch (RuntimeException e) {
+			throw new IllegalCSVFormatException(e);
+		}
+
+		return row;
+	}
+
+
+	static class IllegalCSVFormatException extends RuntimeException {
+		IllegalCSVFormatException(String message) { super(message); }
+		IllegalCSVFormatException(Throwable cause) { super(cause); }
+	}
 }
